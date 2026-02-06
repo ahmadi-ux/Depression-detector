c092324ed7c89a241822fb0b40b81debcce27d2e
Justin-Ott <ottj@mail.gvsu.edu>
2026-02-06 14:41:23 -0500

    Added text description to qwen that it doesn't work

--- vite-project/src/components/sections/DataUpload.jsx
+++ vite-project/src/components/sections/DataUpload.jsx
@@ -66,7 +66,7 @@ export default function DataUpload() {
                       Kimi
                   </DropdownMenuItem>
                   <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Qwen")}>
-                      Qwen
+                      Qwen Doesn't Work 
                   </DropdownMenuItem>
                   <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Compound")}>
                       Compound
