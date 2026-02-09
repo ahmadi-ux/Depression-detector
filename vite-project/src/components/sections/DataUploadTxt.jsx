2a3062810db759c97330ba4163b43b39cc85876b
Justin-Ott <ottj@mail.gvsu.edu>
2026-02-09 09:37:35 -0500

    Deleted most of public as it was from my personal website. Updated app.py and created contactFormTxt and DataUploadTxt.jsx to allow for both text and file upload and analysis

---
+++ vite-project/src/components/sections/DataUploadTxt.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import ContactForm from "../ContactFormTxt";
+import { Button } from "../ui/button";
+import {
+  Dialog, DialogContent, DialogDescription,
+  DialogHeader, DialogTitle, DialogTrigger,
+} from "../ui/dialog";
+import {
+  DropdownMenu, DropdownMenuContent, DropdownMenuGroup,
+  DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu"
+
+const buttonDetails = "rounded-xl shadow-md hover:scale-105 hover:bg-white transition-transform";
+const dropdownMenuStyle = "focus:bg-gray-100 data-[highlighted]:bg-gray-100 cursor-pointer px-2 py-1";
+
+/** Data Upload Section with dialog form for uploading Text
+ * - Dialog with a contact form when button is clicked
+*/
+export default function DataUploadTxt() {
+  const [open, setOpen] = useState(false);
+  const [selectedLLM, setSelectedLLM] = useState("Gemini");
+
+  return (
+    <div className="h-auto flex flex-col items-center justify-center bg-blue-100 p-4 px-8 py-16">
+      <h2 className="text-5xl font-bold mb-4">Text Upload</h2>
+      {/*<div className="w-full max-w-xl flex flex-col">*/}
+
+      <p className="text-lg max-w-xl text-center">
+      </p>
+      <div className="flex justify-center mt-4">
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="lg" className={buttonDetails}>Upload Text</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-md">
+            <DialogHeader>
+              <DialogTitle>Text Upload</DialogTitle>
+              <DialogDescription>
+                Upload text to interface with the LLMs.
+              </DialogDescription>
+            </DialogHeader>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline">Select Model Current Model = {selectedLLM}</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="z-50 bg-white text-black shadow-xl border rounded-md p-1">
+                   <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Gemini")}>
+                      Gemini
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("LLaMA")}>
+                      LLaMA
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("ChatGPT")}>
+                      Chat GPT
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Kimi")}>
+                      Kimi
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Qwen")}>
+                      Qwen Doesn't Work 
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className={dropdownMenuStyle} onClick={() => setSelectedLLM("Compound")}>
+                      Compound
+                  </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <ContactForm onSuccess={() => setOpen(false)} llm={selectedLLM} />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+}
