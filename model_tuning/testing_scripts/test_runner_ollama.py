d6e6edab055870744f273ddd676d7b60da108a52
nstrasler <n.strasler@yahoo.com>
2026-03-18 12:07:55 -0400

    Update testing script for ":" bug

--- model_tuning/testing_scripts/test_runner_ollama.py
+++ model_tuning/testing_scripts/test_runner_ollama.py
@@ -5,27 +5,28 @@ import json
 import pandas as pd
 from datasets import concatenate_datasets
 from datasets import Dataset
-import os
 
 OLLAMA_URL = "http://localhost:11434"
 # Presets for Ollama call
 REQUEST_TIMEOUT_SEC = 300
 NUM_PREDICT = 2200
 TEMPERATURE = 0
-MODEL_NAME = "llama3.1:8b"
+MODEL_NAME = "gpt-oss:20b"
 #llama3.1
-
-
+#gpt-oss:20b
+#emollama:v1
 
 #uniform mapping
 # labels = label, 0 is depressed, all others are not-depressed
 # text = text
-emoDep = pd.read_json('data_sets/combined.json', lines=True)
+
+# Load your CSV
+emoDep = pd.read_json('Dataset/combined.json', lines=True)
 emoDep = emoDep.rename(columns={"label_id": "label"}) #change column name to label
 emoDep['label'] = 0 # Change all values in the 'label_id' column to 0 (depressed)
 emoDep = emoDep.rename(columns={"text": "text"}) #no-op command can change
 
-csv_file1 = pd.read_csv('data_sets/training_data.csv')
+csv_file1 = pd.read_csv('Dataset/training_data.csv')
 csv_file1 = csv_file1.rename(columns={"class": "label"}) #change column name to label
 csv_file1 = csv_file1.rename(columns={"text": "text"}) #no-op command can change
 #remove rows with label value 0 or 4. 0 = depressed, 4 = anxiety
@@ -34,10 +35,10 @@ csv_file1 = csv_file1[~csv_file1["label"].isin([0, 4])]
 csv_file1 = csv_file1[csv_file1["judgment_confidence"] >= .80]
 
 
-RMHD_1 = pd.read_csv('data_sets/labelled_file1.csv')
-RMHD_2 = pd.read_csv('data_sets/labelled_file2.csv')
-RMHD_3 = pd.read_csv('data_sets/labelled_file3.csv')
-RMHD_4 = pd.read_csv('data_sets/labelled_file4.csv')
+RMHD_1 = pd.read_csv('Dataset/labelled_file1.csv')
+RMHD_2 = pd.read_csv('Dataset/labelled_file2.csv')
+RMHD_3 = pd.read_csv('Dataset/labelled_file3.csv')
+RMHD_4 = pd.read_csv('Dataset/labelled_file4.csv')
 RMHD_1['label'] = 0  # Change all values in the 'label' column to 0 (depressed)
 RMHD_2['label'] = 0
 RMHD_3['label'] = 0
@@ -59,7 +60,7 @@ dataset_RMHD_4 = Dataset.from_pandas(RMHD_4)
 
 #13,636 samples total, 6,315 depressed (label 0) and 7,321 not-depressed
 combined_dataset = concatenate_datasets([dataset_csv1, dataset_depEmo, dataset_RMHD_1, dataset_RMHD_2, dataset_RMHD_3, dataset_RMHD_4])
-split_dataset = combined_dataset.train_test_split(test_size=3, seed=42) #remember seed so we can pull out training data.
+split_dataset = combined_dataset.train_test_split(test_size=0.1, seed=42) #remember seed so we can pull out training data.
 test_data = split_dataset["test"]
 
 # Print count of entries with label 0 (depressed)
@@ -133,10 +134,10 @@ for each in test_data:
         FP += 1
         total_ran += 1
         error_result_str = (
-        "FP : "
-        f"Label: {each['label']} | "
-        f"Predicted: {response.strip()} | "
-        f"Text: {each['text']} | "
+            "FP : "
+            f"Label: {each['label']} | "
+            f"Predicted: {response.strip()} | "
+            f"Text: {each['text']} | "
         )  
         results_arr.append(error_result_str)
         print(error_result_str)
@@ -151,18 +152,18 @@ for each in test_data:
         total_ran += 1
     
         error_result_str = (
-        "FN : "
-        f"Label: {each['label']} | "
-        f"Predicted: {response.strip()} | "
-        f"Text: {each['text']} | "
+            "FN : "
+            f"Label: {each['label']} | "
+            f"Predicted: {response.strip()} | "
+            f"Text: {each['text']} | "
         )  
         results_arr.append(error_result_str)
         print(error_result_str)
+
     else:
         print("Received unexpected response: " + response.strip())
         ERROR += 1
         print("Label = " + str(each["label"]))
-        print ("Total ran: " + str(total_ran) + "/" + str(len(test_data)))
         error_result_str = (
             "ERROR : "
             f"Label: {each['label']} | "
@@ -170,8 +171,8 @@ for each in test_data:
             f"Text: {each['text']} | "
         )
         results_arr.append(error_result_str)
-
-
+    print ("Total ran: " + str(total_ran) + "/" + str(len(test_data)))
+    
 # ERROR is safety suicide hotline response from LLM so calculated as TP
 percision = (TP + ERROR) / ((TP + ERROR) + FP)
 recall = (TP + ERROR) / ((TP + ERROR) + FN)
@@ -182,23 +183,12 @@ f1_score = 2 * (percision * recall) / (percision + recall)
 results_arr.insert(0, "TP: " + str(TP) + " FP: " + str(FP) + " TN: " + str(TN) + " FN: " + str(FN) + " ERROR: " + str(ERROR)
                    + " Precision: " + str(percision) + " Recall: " + str(recall) + " Accuracy: " + str(accuracy) + " F1 Score: " + str(f1_score) + " Model: " + MODEL_NAME)
 
-
-def make_safe_filename(value):
-    # For when model name has invalid naming char
-    invalid_chars = '<>:"/\\|?*'
-    safe = "".join("_" if c in invalid_chars else c for c in value)
-    return safe.strip(" .")
-
 # Generate a datetime string for the filename
-safe_model_name = make_safe_filename(MODEL_NAME)
-dt_str = datetime.now().strftime(f"{safe_model_name}_%m%d_%H%M%S")
-results_folder = "results"
-results_path = os.path.join(results_folder, f"{dt_str}.txt")
-os.makedirs(results_folder, exist_ok=True)
-with open(results_path, "w", encoding="utf-8") as f:
+dt_str = datetime.now().strftime(f"MODEL_{MODEL_NAME.replace(':', '_')}_%m%d_%H%M%S")
+results_filename = f"results_{dt_str}.txt"
+with open(results_filename, "w", encoding="utf-8") as f:
     for line in results_arr:
         f.write(line + "\n")
-print(f"Saved results to: {results_path}")
 
 
 print(f"Precision: {percision:.4f}")
